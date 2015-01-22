5c57c64d7f6b3dd71f4f961983134047431a5f4b
Federico Javier Lopez Bertoni <federico@greenlight.com.py>
2015-01-22 11:39:49 -0300

    Cleanup before starting.

--- tests/preprocessing/heartlungssegmentation.m
+++ tests/preprocessing/heartlungssegmentation.m
@@ -1,6 +1,10 @@
 % Pre-processing steps to separate heart from lungs. 
- 
+clear all  clc 
 % Preliminary steps common to all methods.
 % Step 1 == Load data
 % variable name is vol1
 load sampledata 
+
+% ======================================
+% Method that separates heart from lungs using tipping points
+% Yan Yang's thesis. 
