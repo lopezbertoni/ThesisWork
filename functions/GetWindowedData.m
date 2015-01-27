777b368a4919c224b446f962fb9f153e55a86889
Federico Javier Lopez Bertoni <federico@greenlight.com.py>
2015-01-27 17:48:41 -0300

    Initalizations.

---
+++ functions/GetWindowedData.m
@@ -0,0 +1,9 @@
+function Vwin = GetWindowedData(V, range, win_width, win_center, rescale_int, rescale_slope)
+
+Vrs = (V*rescale_slope) + rescale_int;
+Vbot = (Vrs <= (win_center - 0.5 - 0.5*(win_width - 1)));
+Vtop = (Vrs > (win_center - 0.5 + 0.5*(win_width - 1)));
+Vmid = ( ((Vrs - (win_center - 0.5))/(win_width - 1) + 0.5)*(range(2) - range(1)) + range(1) );
+Vwin =  (~(Vbot + Vtop)).*Vmid + Vtop.*range(2) + Vbot.*range(1);
+
+end
--- tests/preprocessing/heartlungssegmentation.m
+++ tests/preprocessing/heartlungssegmentation.m
@@ -1,12 +1,37 @@
-% Pre-processing steps to separate heart from lungs. 
-clear all  clc 
+% Pre-processing steps to separate heart from lungs.
+clear all  clc
 % Preliminary steps common to all methods.
 % Step 1 == Load data
 % variable name is vol1
-load sampledata 
+load sampledata
 datasize = size(vol1);
+
+% Hounsfield chose a scale that affects the four basic densities, with the following values:
+% 
+% Air = - 1000
+% Fat = -60 to -120
+% Water = 0
+% Compact bone = +1000
+
+% Window data parameters 
+% from DICOM headers
+win_w = 350;
+win_c = 35;
+rescale_i = -1024;
+rescale_s = 1;
+range = [0 255];
+vol1win = GetWindowedData(vol1, range, win_w, win_c, rescale_i, rescale_s);
 
 % ===========================================================
 % Method that separates heart from lungs using tipping points
-% Yan Yang's thesis. 
+% Yan Yang's thesis.
 
+slicenumber = 25;
+
+if(slicenumber > 0)
+    %     Process a single slice
+    I0 = vol1(:,:,slicenumber);
+    
+else
+    %     Proceess whole volume
+end
